b81fd1d45de95cba3563de1966d38b449990b266
aiza_verzon <verzonaiza01@gmail.com>
2025-10-21 18:37:47 +0800

    Add Portfolio

--- app/dashboard/AizaVerzon/page.tsx
+++ app/dashboard/AizaVerzon/page.tsx
@@ -1,8 +1,20 @@
 export default function Page() {
-    return(
+    return (
         <>
+        <main className="p-8">
+        <h1 className="text-3xl font-bold mb-4">My Portfolio</h1>
+        <p>Check out my other project on GitHub 👇</p>
 
+        <a
+          href="https://github.com/AizaVerzon/newportfolio.git"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-blue-600 hover:underline"
+        >
+          View My Other Repository →
+        </a>
+      </main>   
         </>
-
-    )
+    );
+       
 } 
