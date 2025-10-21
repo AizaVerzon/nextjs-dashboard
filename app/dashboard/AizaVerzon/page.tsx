d330cf2a9806d3f8d97abd7d7779c27005e41f5e
aiza_verzon <verzonaiza01@gmail.com>
2025-10-21 19:28:05 +0800

    added aiza page

--- app/dashboard/AizaVerzon/page.tsx
+++ app/dashboard/AizaVerzon/page.tsx
@@ -1,19 +1,17 @@
+"use client";
 export default function Page() {
+
     return (
         <>
-        <main className="p-8">
-        <h1 className="text-3xl font-bold mb-4">My Portfolio</h1>
-        <p>Check out my other project on GitHub 👇</p>
-
-        <a
-          href="https://github.com/AizaVerzon/newportfolio.git"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="text-blue-600 hover:underline"
-        >
-          View My Other Repository →
-        </a>
-      </main>   
+ <main className="p-6">
+      <div className="border rounded-xl shadow-md overflow-hidden">
+        <iframe
+          src="https://aizaverzon.github.io/newportfolio/"
+          title="Aiza Verzon Portfolio"
+          className="w-full h-[90vh]"
+        />
+      </div>
+    </main>
         </>
     );
        
